a17758a6ee82c73336af08de7d9690a642d206fa
Kesheng Chen (Wicresoft) <v-kesche@microsoft.com>
2018-08-13 15:38:52 +0800

    2018081302

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 let express = require('express');
 let uuid = require('uuid');
 let path = require('path');
+let mysql = require('mysql');
 let functions = require('./functions.js');
 let bodyParser = require('body-parser');
 let multer = require('multer');
@@ -88,3 +89,75 @@ app.post("/login",(req,res,next)=>{
 });
 
 app.listen(8080);
+
+//functions
+function functions (){
+    let newui=()=>{return {
+        Id:-1,
+        Alias:'',
+        UserName:'',
+        PassWord:'',
+        IsAdmin:0,
+        HeadPhoto:''
+    }};
+    this.connection = mysql.createConnection({
+        host:"localhost",
+        port:3306,
+        user:"root",
+        password:"123147",
+        database:"testWebsite"
+    })
+    this.sql=new SQL();
+    this.SignUp=(req,res,next)=>{
+        let ui=newui();
+        ui.Alias = req.body.Alias;
+        ui.UserName = req.body.UserName;
+        ui.PassWord = req.body.PassWord;
+        let sql = this.sql.SignUp(ui);
+        this.connection.query(sql,(err, result)=>{
+            console.log(result);
+        })
+        sql = this.sql.Login(ui.Alias,ui.PassWord);
+        this.connection.query(sql,(err, result)=>{
+            console.log(result);
+        })
+
+       
+    };
+}
+//SQL
+functionSQL() {
+    this.CheckUserNameExist = (alias)=>{
+        return "SELECT CASE WHEN COUNT(*) = 1 THEN TRUE ELSE FALSE END AS Exist FROM UserInfo WHERE Alias = '"+alias+"'";
+    }
+    this.SignUp = (ui)=>{
+        return "INSERT INTO UserInfo(Alias,UserName,PassWord) VALUES('"+ui.Alias+"','"+ui.UserName+"','"+ui.PassWord+"')";
+    }
+    this.Login=(alias,password)=>{
+        return "SELECT * FROM UserInfo WHERE Alias='"+alias+"' AND PassWord = '"+password+"'";
+    }
+    this.UploadHeadImage=(ui,headimage)=>{
+        return "UPDATE UserInfo SET HeadPhoto='"+headimage+"' WHERE Id="+ui.Id;
+    }
+    this.ChangeUserInfo=(oldui,newui)=>{
+        var sets=[];
+        for(var prop in newui){
+            if(oldui[prop]!=newui[prop] && prop!='Id'){
+                if(Number ==typeof newui[prop]){
+                    sets.push(prop+" = "+newui[prop]);
+                }else{
+                    sets.push(prop+" = '"+newui[prop]+"'");
+                }
+            }
+        }
+        if(sets.length>0){
+            return "UPDATE UserInfo SET "+sets.join(',');
+        }else{
+            return undefined;
+        }
+    }
+
+   // app.get("/CheckLogin",(req,res,next)=>{});
+    //app.get("/UploadPhoto",(req,res,next)=>{});
+    //app.get("/GetPhotoList",(req,res,next)=>{});
+}
